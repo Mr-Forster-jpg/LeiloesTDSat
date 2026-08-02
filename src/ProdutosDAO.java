2f798c89b9b9626e190f1b66409184535aaa2924
Ivan Forster <ivan.s.forster@gmail.com>
2026-08-01 22:52:27 -0300

    Adicionando a funcionalidade do botão Vender

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -40,33 +40,53 @@ public class ProdutosDAO {
     public ArrayList<ProdutosDTO> listarProdutos() {
         conn = new conectaDAO().connectDB();
         String sql = "SELECT * FROM produtos";
-        
+        ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+    
         try {
             prep = conn.prepareStatement(sql);
             resultset = prep.executeQuery();
-            
+
             while (resultset.next()) {
                 ProdutosDTO produto = new ProdutosDTO();
                 produto.setId(resultset.getInt("id"));
                 produto.setNome(resultset.getString("nome"));
                 produto.setValor(resultset.getInt("valor"));
                 produto.setStatus(resultset.getString("status"));
-                
+
                 listagem.add(produto);
+                }
+            } catch (SQLException erro) {
+                JOptionPane.showMessageDialog(null, "Erro ao listar produtos vendidos: " + erro.getMessage());
+            } finally {
+                try {
+                    if (resultset != null) resultset.close();
+                    if (prep != null) prep.close();
+                    if (conn != null) conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
-            
-        } catch (SQLException erro) {
-            JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + erro.getMessage());
+            return listagem;
+        }
+    
+    public void venderProduto(int id) {
+    String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+    conn = new conectaDAO().connectDB();
+    
+        try {
+            prep = conn.prepareStatement(sql);
+            prep.setInt(1, id);
+            prep.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Produto vendido com sucesso!");
+            } catch (SQLException erro) {
+                JOptionPane.showMessageDialog(null, "Erro ao vender produto: " + erro.getMessage());
         } finally {
             try {
-                if (resultset != null) resultset.close();
                 if (prep != null) prep.close();
                 if (conn != null) conn.close();
             } catch (SQLException e) {
-                JOptionPane.showMessageDialog(null, "Erro ao fechar conexão: " + e.getMessage());
+                e.printStackTrace();
             }
         }
-        
-        return listagem;
     }
 }
